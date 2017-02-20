144d5fde78da8493553ac2f9a37a77f025c48251
ipanhiyat <ipanhidayat123@gmail.com>
2017-02-19 20:44:37 -0800

    https://github.com/ipanhiyat/Ipanhidayat_14111136_UTS.git

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function(){
+	return view('home');
+});
+
+Route::get('/halamanawal', function(){
+	return view('halamanawal');
+});
+
+Route::get('/edit', function(){
+	return view('edit');
+});
+
+Route::get('/tambah', function(){
+	return view('tambah');
+});
+
